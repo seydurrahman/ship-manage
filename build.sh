e3ffe0de3e7ab63ec042a7e2da65e6ac4073061b
Seydur <seydur.rahman82@gmail.com>
2025-12-06 15:36:19 +0600

    settings doe1e

--- build.sh
+++ build.sh
@@ -2,11 +2,9 @@ set -e errexit
 
 pip install -r requirements.txt
 
-<<<<<<< HEAD
 python manage.py collectstatic --no-input
 =======
 python manage.py collectstatic --noinput
->>>>>>> d2f6c12cf59e5672338aeec2b5dbb02790d57f80
 python manage.py migrate
 
 if [[$CREATE_SUPERUSER == "true"]]; then
