816c08ab222627f525a4ce7fe18e6d200928cf30
Seydur <seydur.rahman82@gmail.com>
2025-12-16 12:37:33 +0600

    last

--- ship-backend/build.sh
+++ ship-backend/build.sh
@@ -1,10 +1,6 @@
-set -e errexit
+#!/usr/bin/env bash
+set -e
 
 pip install -r requirements.txt
-
 python manage.py collectstatic --noinput
 python manage.py migrate
-
-# if [[$CREATE_SUPERUSER == "true"]]; then
-#     python manage.py createsuperuser --noinput
-# fi
